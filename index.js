3abc62bc49ced717fb03415f248f3b01580dd194
anhhuy <phamanhhuy22@gmail.com>
2023-12-07 15:05:01 +0700

    test

--- index.js
+++ index.js
@@ -4,31 +4,30 @@ const cors = require("cors");
 app.use(cors());
 const route = require("./routes/index");
 const swaggerUI = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerJsDoc = require("./public/swagger.json");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// const options = {
+// 	definition: {
+// 		openapi: "3.0.0",
+// 		info: {
+// 			title: "Book API",
+// 			version: "1.0.0",
+// 			description: "A simple Express Book API",
+// 		},
+// 		servers: [
+// 			{
+// 				url: "https://foodbook-api-nodejs.vercel.app/api",
+// 			},
+// 		],
+// 	},
+// 	apis: ["./routes/*.js"],
+// };
 
-const options = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Book API",
-			version: "1.0.0",
-			description: "A simple Express Book API",
-		},
-		servers: [
-			{
-				url: "https://foodbook-api-nodejs.vercel.app/api",
-			},
-		],
-	},
-	apis: ["./routes/*.js"],
-};
+// const specs = swaggerJsDoc(options);
 
-const specs = swaggerJsDoc(options);
-
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc));
 route(app);
 //database connect
 const db = require("./config/db");
--- routes/recipe.js
+++ routes/recipe.js
@@ -1,6 +1,6 @@
 /**
- * @swagger
- * tags:
+* @swagger
+ tags:
  *   name: Recipe
  *   description: Operations related to recipes
  *
